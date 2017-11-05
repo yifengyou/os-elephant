f016ae0019a7fcd452bfd2ec66a4b0083ff79871
youyifeng <842056007@qq.com>
2017-11-05 23:07:48 +0800

    20171105-02

--- code/c10/b/kernel/main.c
+++ code/c10/b/kernel/main.c
@@ -6,13 +6,17 @@
 
 void k_thread_a(void*);
 void k_thread_b(void*);
+void k_thread_c(void*);
+void k_thread_d(void*);
 
 int main(void) {
    put_str("I am kernel\n");
    init_all();
 
-   thread_start("k_thread_a", 31, k_thread_a, "argA ");
-   thread_start("k_thread_b", 8, k_thread_b, "argB ");
+   thread_start("k_thread_a", 64, k_thread_a, "argA ");
+   thread_start("k_thread_b", 32, k_thread_b, "argB ");
+   thread_start("k_thread_c", 16, k_thread_c, "argC ");
+   thread_start("k_thread_d", 8, k_thread_d, "argD ");
 
    intr_enable();
    while(1) {
@@ -38,3 +42,23 @@ void k_thread_b(void* arg) {
       console_put_str(para);
    }
 }
+
+/* 在线程中运行的函数 */
+void k_thread_c(void* arg) {     
+/* 用void*来通用表示参数,被调用的函数知道自己需要什么类型的参数,自己转换再用 */
+   char* para = arg;
+   while(1) {
+      console_put_str(para);
+   }
+}
+
+/* 在线程中运行的函数 */
+void k_thread_d(void* arg) {     
+/* 用void*来通用表示参数,被调用的函数知道自己需要什么类型的参数,自己转换再用 */
+   char* para = arg;
+   while(1) {
+      console_put_str(para);
+   }
+}
+
+
